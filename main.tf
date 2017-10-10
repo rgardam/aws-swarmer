de8b312b55588a0988851b6b95a18274198bec27
Robert Gardam <robert.gardam@native-instruments.de>
2017-10-10 17:54:01 +0200

    added support for multiple ssh keys for docker user added names and tags to resources

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "random_id" "key" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "swarm"
+  name = "swarm-${var.name}"
 
   cidr = "${var.vpc_cidr_block}"
 
@@ -54,13 +54,23 @@ module "vpc" {
   single_nat_gateway = true
 
   enable_s3_endpoint = "${var.enable_s3_endpoint}"
+
+  tags = {
+    Terraform = "true"
+    Name = "${var.name}"
+  }
 }
 
 ### Security Groups ### 
 resource "aws_security_group" "swarm" {
-  name        = "swarm"
+  name        = "swarm-${var.name}"
   description = "Security group for swarm cluster instances"
   vpc_id      = "${module.vpc.vpc_id}"
+
+  tags = {
+    Terraform = "true"
+    Name = "swarm-${var.name}"
+  }
 
   # allow all internal VPC traffic
   ingress {
@@ -111,15 +121,16 @@ resource "aws_security_group" "swarm" {
       "0.0.0.0/0",
     ]
   }
-
-  tags {
-    Name = "swarm"
-  }
 }
 
 ### KMS Setup ###
 resource "aws_kms_key" "swarm_key" {
   description = "KMS key for whole environment secrets"
+
+  tags = {
+    Terraform = "true"
+    Name = "swarm-${var.name}"
+  }
 }
 
 resource "aws_kms_alias" "swarm_key_alias" {
@@ -127,12 +138,11 @@ resource "aws_kms_alias" "swarm_key_alias" {
   target_key_id = "${aws_kms_key.swarm_key.key_id}"
 }
 
-### Template fillers ###
 data "template_file" "cloud_init_bootstrap" {
   template = "${file("${path.module}/templates/cloud-init-bootstrap.yml.tmp")}"
 
   vars {
-    ssh_public_keystring = "${var.ssh_public_keystring}"
+    ssh_public_keystrings = "${join("\n      - ", var.ssh_public_keystrings)}"
     random_id            = "${random_id.key.hex}"
     region               = "${var.region}"
   }
@@ -142,7 +152,7 @@ data "template_file" "cloud_init_manager" {
   template = "${file("${path.module}/templates/cloud-init-joiner.yml.tmp")}"
 
   vars {
-    ssh_public_keystring = "${var.ssh_public_keystring}"
+    ssh_public_keystrings = "${join("\n      - ", var.ssh_public_keystrings)}"
     random_id            = "${random_id.key.hex}"
     region               = "${var.region}"
     manager_ip           = "${aws_instance.bootstrap_manager.private_ip}"
@@ -154,7 +164,7 @@ data "template_file" "cloud_init_worker" {
   template = "${file("${path.module}/templates/cloud-init-joiner.yml.tmp")}"
 
   vars {
-    ssh_public_keystring = "${var.ssh_public_keystring}"
+    ssh_public_keystrings = "${join("\n      - ", var.ssh_public_keystrings)}"
     random_id            = "${random_id.key.hex}"
     region               = "${var.region}"
     manager_ip           = "${aws_instance.bootstrap_manager.private_ip}"
@@ -175,7 +185,7 @@ data "template_file" "instance_profile" {
 
 ### IAM Instance Policy ###
 resource "aws_iam_role" "swarm" {
-  name = "TF_swarm_instance_profile"
+  name = "TF_swarm_instance_profile-${var.name}"
 
   assume_role_policy = <<EOF
 {
@@ -195,12 +205,12 @@ EOF
 }
 
 resource "aws_iam_instance_profile" "swarm" {
-  name = "TF-Swarm-instance_profile"
+  name = "TF-Swarm-instance_profile-${var.name}"
   role = "${aws_iam_role.swarm.name}"
 }
 
 resource "aws_iam_role_policy" "swarm" {
-  name   = "TF_swarm_instance_profile"
+  name   = "TF_swarm_instance_profile-${var.name}"
   role   = "${aws_iam_role.swarm.name}"
   policy = "${data.template_file.instance_profile.rendered}"
 }
@@ -215,11 +225,16 @@ resource "aws_instance" "bootstrap_manager" {
   associate_public_ip_address = true
   vpc_security_group_ids      = ["${aws_security_group.swarm.id}"]
   iam_instance_profile        = "${aws_iam_instance_profile.swarm.name}"
+
+  tags = {
+    Terraform = "true"
+    Name = "swarm-${var.name}"
+  }
 }
 
-### Swarm Manager Manager Instances ###
+# ### Swarm Manager Manager Instances ###
 resource "aws_instance" "additional_managers" {
-  count                       = "1"
+  count                       = "${var.additional_manager_count}"
   ami                         = "${data.aws_ami.amazon_linux.id}"
   instance_type               = "${var.manager_instance_size}"
   subnet_id                   = "${element(module.vpc.private_subnets, count.index)}"
@@ -227,13 +242,18 @@ resource "aws_instance" "additional_managers" {
   associate_public_ip_address = false
   vpc_security_group_ids      = ["${aws_security_group.swarm.id}"]
   iam_instance_profile        = "${aws_iam_instance_profile.swarm.name}"
+
+  tags = {
+    Terraform = "true"
+    Name = "swarm-${var.name}"
+  }
 }
 
 ### Launch configuration and autoscaling group for swarm worker
 module "swarm_worker_asg" {
   source = "terraform-aws-modules/autoscaling/aws"
 
-  lc_name = "swarm-worker-lc"
+  lc_name = "swarm-worker-lc-${var.name}"
 
   image_id        = "${data.aws_ami.amazon_linux.id}"
   instance_type   = "${var.worker_instance_size}"
@@ -249,24 +269,42 @@ module "swarm_worker_asg" {
   ]
 
   # Auto scaling group
-  asg_name                  = "swarm-worker-asg"
+  asg_name                  = "swarm-worker-asg-${var.name}"
   vpc_zone_identifier       = ["${module.vpc.private_subnets}"]
   health_check_type         = "EC2"
   min_size                  = "${var.worker_min_asg_size}"
   max_size                  = "${var.worker_max_asg_size}"
   desired_capacity          = "${var.worker_desired_asg_size}"
   wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Terraform"
+      value               = "True"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Name"
+      value               = "swarm-worker-${var.name}"
+      propagate_at_launch = true
+    }
+  ]
 }
 
 ### Swarm Worker ELB ###
 module "elb" {
   source = "terraform-aws-modules/elb/aws"
 
-  name = "swarm-elb"
+  name = "swarm-elb-${var.name}"
 
   subnets         = ["${module.vpc.public_subnets}"]
   security_groups = ["${aws_security_group.swarm.id}"]
   internal        = false
+
+  tags = {
+    Terraform      = "True"
+    Name = "${var.name}"
+  }
 
   listener = [
     {
--- variables.tf
+++ variables.tf
@@ -1,6 +1,12 @@
 ### Basic config
-variable "ssh_public_keystring" {
-  description = "public ssh key for docker user"
+variable "name" {
+  description = "the name of the deployment. This is used to distinguish resources"
+}
+
+variable "ssh_public_keystrings" {
+  type = "list"
+  description = "public ssh keys for docker user"
+  default = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUFWdnTi8nrmkH2AAgJ26Mw4iJgLGuwYg6FDkujZHccv2B48hXnVMugh+iFJbPgg9ZxtizxCvsHL2RgpGRk0SzNfDGhEcKZBPCmmilid/wE7p2CKzwy8MwlxV9YO7HlvEkJaDa3GTbDl5/RLpKUyZZCK48FVlMYYsWv/SXCF0VwYLHQA3VnhgEhFqZcBOoHHSzOgYh6fTA9C5V7WpY7Wi76xLClZ6i8C7Qf1Aoh5k2iPdOFapy8lQ8n6mh7GpMb5t1RWUAT1pJopeBY5zOqrmfqhRgWYhZ8Dg9RbEbb84OL9Wa0qPR9vGIQHKXfTwTrDoUNga6QG+eAO2ybQJXYKth robertgardam@ni-bln-04512.local", "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/zwes2ZoXdBn304IwueGP0DzJH+8eHHeU1ff6kuDxT4ImK5JVWGdQj/BaXx77HYloC2ORpx7UXyguf82FsNQPQ0LcalNbPLWT74m9HAgG91gs/7uVhmG1Lgt1P5PUQ0vt+LGVxX4ItixzJgl64ItsVlvhhJ4r1b0hU/ELPAXzB9OMMBliJM7fvpgC+i6dJu4jh8YwIIzbwb3+vNzbqx5rh/K0E9KhjM43GMD6OzHh0m/B5zneWM72SUN3GGzNlkaugZROHMlhpm38Q7Fy5xlYdLx0Rxo+mJdAlmffcsCXm+hnZPDSC0BBNOcem6T/9wFQ9LrRrsgFkM1TrAplgKeT chris@SWS-QA-MacMini-3.local", "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC100a7N4B3oSiH+EnQBPvE4xDWJsFYWEmlssQU2+4AFLUhK7cJZAHoCpjDrLqdle7W40B30caS8ZosWZlu/is2SGyixJyeEYMJdT+7yZ/GXt70W7qqmuey5nvM7dhxePFwb/c0ZFZcCHSdvdyGB4j3A5fLAt70gMk7HnfdoU9h1nnzlruIFdHR92QjFeIKrhEM/+iPeAq0OmEiGuZr8nPKBIUrFIDTULQWIHbHYorIUmPqiDLlf5XJH1oIVNbExOfNGN2hjb0ewBySyiFgoxQ5vjldAdToNTlUeMk9PnA5bvxfi/L93UtznqqIGzv7RApABwl6uaI3joFj7asTIYIJ francois.rose@native-instruments.de", "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+a9NzbAkX2kSFITk0k/rUlVAWsqNldmNDh/RJ/hEW0No6re6z1jUr6KQuhOB7k1fKgRbzyra7DfSdWBB8SML1lvIMnDEXrf/SthdEMLpovrQVzIdK7oTmlMc5labj9w3W1ps7inpEuqDhM9Wab04ne+W89ZebSIXj3WDThlpG1GR36Rcio4sVTYAZNZosTOcaPaaOrD8/ak8BehisyAqAUvWD0OIAQiqwzVrVuSXYmH4sJzMwx2eMPbkt8yK+Tw8xNwoK6kkYfGzEjCKaj7TkIrSMwe0kwUHjtA5qdITQTeqwpLysIjFYZ6eViZmU7Vr9QWcOqV70F+sGA0TjHK6z martinho.fernandes@ni-bln-05929.local"]
 }
 
 variable "region" {
@@ -50,6 +56,11 @@ variable "manager_instance_size" {
 variable "worker_instance_size" {
   description = "the instance size of the swarm worker"
   default = "t2.micro"
+}
+
+variable "additional_manager_count" {
+  description = "the number of additional manager instances to launch"
+  default = 1
 }
 
 variable "worker_min_asg_size" {
